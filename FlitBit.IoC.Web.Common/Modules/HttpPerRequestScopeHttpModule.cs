05e4aa02cefa5bf7380db5e0a6f12b8717eb8194
Tyler Garlick <tjgarlick@gmail.com>
2013-12-10 12:05:55 -0500

    add http module fixes

---
+++ FlitBit.IoC.Web.Common/Modules/HttpPerRequestScopeHttpModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using FlitBit.Core;
+
+namespace FlitBit.IoC.Web.Common.Modules
+{
+    public class HttpPerRequestScopeHttpModule : IHttpModule
+    {
+        IContainer _container;
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (sender, args) =>
+                                    {
+                                        _container = Container.Current.MakeChildContainer(CreationContextOptions.InstanceTracking);
+                                        ContainerHelpers.Current = _container;
+                                    };
+        }
+
+        public void Dispose()
+        {
+            Util.Dispose(ref _container);
+        }
+    }
+}
